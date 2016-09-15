6fe90fd1a11259a2068b3cc1f081eb579af40cbf
Roman Vasilev <s91630277@gmail.com>
2016-09-15 15:11:12 +0300

    Playing with start/kill

--- SublimeNodeServer.py
+++ SublimeNodeServer.py
@@ -1,154 +1,54 @@
-import getpass
-import queue
+import sublime
+import sublime_plugin
 import os
-import platform
-import socket
 import subprocess
-import sys
-import time
 import threading
 
-import sublime
+# server_path = os.path.join(sublime.packages_path(), os.path.printname(os.path.realpath(__file__)), "node-server.js")
+package_path = os.path.dirname(os.path.realpath(__file__));
+server_path = "node-server.js"
+server_address = "127.0.0.1"
+server_port = "7093"
 
-SERVER_ADDRESS = "127.0.0.1"
-SERVER_PORT = 7093
-SERVER_PATH = os.path.join(sublime.packages_path(), os.path.dirname(os.path.realpath(__file__)), "node-server.js")
+server_thread = None
+server_proc = None
+
+def server_process():
+    global server_proc, server_thread
+    if os.name == "nt":
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.SW_HIDE | subprocess.STARTF_USESHOWWINDOW
+        server_proc = subprocess.Popen(["node", server_path, server_address, server_port], cwd=package_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, startupinfo=si)
+    else:
+        server_proc = subprocess.Popen(["node", server_path, server_address, server_port], cwd=package_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    print('server_proc')
+    print(server_proc)
+    outs, errs = server_proc.communicate()
+    print(outs)
+    print(errs)
+    print('server_process server_proc.pid', server_proc.pid)
 
 def plugin_loaded():
-    # Called when the Sublime Text API is ready for use
-    print("ready")
-
-    # server = SublimeNodeServer(SERVER_ADDRESS, SERVER_PATH)
-    # server.start()
-    # SublimeNodeServer.thread.start()
-
-    client = NodeThreadingClient()
-    client.start()
-    # client.send("Hello")
-    # client.end()
-
-
-class NodeThreadingClient(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self, name = "NodeThreadingClient")
-        self.connected = False
-        self.socket = socket.socket()
-        self.socket.connect((SERVER_ADDRESS, SERVER_PORT))
-        # self.connected = True
-
-    def send(self, s):
-        data = bytes(str(s))
-        self.socket.send(data, 'UTF-8')
-        data = self.socket.recv(16 * 1024)
-        print('Received:', repr(data))
-        
-    def end(self):
-        self.socket.close()
-        self.connected = False
-
+    global server_proc, server_thread
+    print("plugin_loaded")
+    server_thread = threading.Thread(target=server_process, args=())
+    server_thread.start()
+    print('After server_thread')
+    print('server_proc', server_proc)
+    print('server_proc.pid', server_proc.pid)
 
 def plugin_unloaded():
-    # Called just before the plugin is unloaded.
-    if SublimeNodeServer.thread:
-        SublimeNodeServer.thread.terminate()
-
-class SublimeNodeServer(threading.Thread):
-    # Manages the node server and printing its output.
-
-    thread = None
-
-    def __init__(self, server_address, server_path):
-        threading.Thread.__init__(self, name="SublimeNodeServerThread")
-        self.server_address = server_address
-        self.server_path = server_path
-        self.child = None
-        self.client = SublimeNodeClient(self.server_address)
-
-    def run(self):
-        env = os.environ.copy()
-        try:
-            child = subprocess.Popen(
-                ["node", self.server_path, self.server_address],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                env=env
-            )
-            self.child = child
-        except OSError:
-            raise Exception(
-                "Couldn't find `node` in `{0}`.".format(env["PATH"])
-            )
-
-        self.client.start()
-
-        while child.poll() is None:
-            stdout = child.stdout.readline().decode("utf-8")
-            sys.stdout.write(stdout)
-
-        stdout, stderr = child.communicate()
-        stdout = stdout.decode("utf-8")
-        stderr = stderr.decode("utf-8")
-        if stdout:
-            sys.stdout.write(stdout)
-        if stderr:
-            message = "Node server encountered an error.\n{0}".format(
-                "\n".join(["> " + line for line in stderr.split("\n")])
-            )
-            raise Exception(message)
-
-    def terminate(self):
-        """Sends SIGINT to the node child process."""
-        if self.client:
-            self.client.terminate()
-            self.client = None
-        if self.child.poll() is None:
-            self.child.terminate()
-            self.child = None
-
-class SublimeNodeClient(threading.Thread):
-    """Manages communication with the node server."""
-
-    BRIDGE_THROTTLE = 0.01
-    CONNECT_TIMEOUT = 30
-
-    def __init__(self, server_address):
-        threading.Thread.__init__(self, name="SublimeNodeClientThread")
-        self.server_address = server_address
-        self.connected = False
-        self.queue = queue.Queue()
-
-    def run(self):
-        client = socket.socket()
-        client.connect((SERVER_ADDRESS, SERVER_PORT))
-        end_time = time.time() + SublimeNodeClient.CONNECT_TIMEOUT
-        while not self.connected:
-            remaining_time = end_time - time.time()
-            if remaining_time < 0:
-                raise Exception(
-                    "Unable to connect to `{0}`".format(self.server_address)
-                )
-            try:
-                client.connect(self.server_address)
-                self.connected = True
-            except (ConnectionRefusedError, FileNotFoundError):
-                pass
-
-        while self.connected:
-            while not self.queue.empty():
-                (message, callback) = self.queue.get()
-                encoded = sublime.encode_value(message) + "\n"
-                client.send(bytes(encoded, "utf-8"))
-                if callback:
-                    sublime.set_timeout(callback, 0)
-            time.sleep(SublimeNodeClient.BRIDGE_THROTTLE)
-
-        client.close()
-
-    def send(self, message, callback=None):
-        """Sends a message to the node server."""
-        self.queue.put((message, callback))
-
-    def terminate(self):
-        """Disconnects from the node server and terminates this thread."""
-        self.connected = False
+    global server_proc, server_thread
+    print("plugin_unloaded")
+    if (server_proc):
+        server_proc.kill()
+        print('server_proc')
+        print(server_proc)
+    
+class TestNodeServerTextCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        global server_proc, server_thread
+        print(server_proc)
+        print('server_proc.pid', server_proc.pid)
+        print(server_thread)
+        server_proc.kill()
--- node-server.js
+++ node-server.js
@@ -1,28 +1,30 @@
-'use strict';
+"use strict";
 
-const net = require('net');
+const net = require("net");
 
-const SERVER_PORT = process.argv[2] || 7093;
+const SERVER_ADDRESS = process.argv[2] || "127.0.0.1";
+const SERVER_PORT = process.argv[3] || 7093;
 
 const server = net.createServer()
-  .on('connection', socket => {
-    console.log('Connected: %j', socket.remoteAddress);
+  .on("connection", socket => {
+    console.log("Connected: %j", socket.remoteAddress);
     var buffer = new Buffer(0);
-    socket.on('data', chunk => {
+    socket.on("data", chunk => {
       buffer = Buffer.concat([buffer, chunk]);
-      console.log('Received: %j', chunk.toString());
-      console.log('Buffer: %j', buffer.toString());
-      socket.write("Packet: " + Date.now() + "\n");
-      // socket.end();
+      console.log("Received: %j", chunk.toString());
+      // TODO: Parse JSON.
+      socket.write("Server response: " + new Date() + "\n");
+      socket.end();
     });
     socket.on("error", err => {
       console.log("Socket error:", err);
     });
   })
-  .on('error', error => {
-    console.log(error);
+  .on("error", err => {
+    console.log("Server error:", err.message);
   })
-  .listen(SERVER_PORT, () => {
+  .listen(SERVER_PORT, SERVER_ADDRESS, (err) => {
     const address = server.address();
     console.log('Listening: %j', address);
+    // server.close(); // Shutdowns the server
   });
